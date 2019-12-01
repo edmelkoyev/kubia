7a9d0527d0bcab898d9bad399a5938b92d05c0ff
Edward Melkoyev <edmelkoyev@wiley.com>
2019-12-01 22:39:16 +0300

    ch04: healthcheck for pod by Kubelet

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const os = require('os');
 
 console.log("Kubia server starting...");
 
-var handler = function(request, response) {
-  console.log("Received request from " + request.connection.remoteAddress); 
+let requestCount = 0;
+
+const handler = function(request, response) {
+  console.log("Received request #" + requestCount + " from " + request.connection.remoteAddress);
+  requestCount++;
+
+  if (requestCount > 5) {
+    response.writeHead(500);
+    response.end("I'm not well. Please restart me!");
+    return;
+  }
   response.writeHead(200);
   response.end("You've hit " + os.hostname() + "\n");
 };
 
-var www = http.createServer(handler); 
+const www = http.createServer(handler);
 www.listen(8080);
